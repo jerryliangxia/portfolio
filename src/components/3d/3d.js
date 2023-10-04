782800ce122c8562b8a91d0aa5f465062b8ab3b4
Jerry Xia <jerryxia@Jerrys-MacBook-Pro.local>
2023-10-03 20:35:29 -0400

    Add Heap for analytics, image rotation for Spider-Man FS

--- src/components/3d/3d.js
+++ src/components/3d/3d.js
@@ -1,3 +1,4 @@
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import SpidermanShowcase from "./3dComponents/SpidermanShowcase";
 import { Flex, Text } from "@radix-ui/themes";
@@ -8,6 +9,25 @@ import YouTubeBlock from "./3dComponents/YouTubeBlock";
 
 function ThreeD() {
   const isMobile = window.innerWidth <= 768;
+  const imgRef = useRef();
+  const images = [
+    "images/3d/ps5_full_suit/portrait_annotated.png",
+    "images/3d/ps5_full_suit/90.png",
+    "images/3d/ps5_full_suit/180.png",
+  ];
+  const [currentImage, setCurrentImage] = useState(images[0]);
+
+  const handleMouseMove = (e) => {
+    const rect = imgRef.current.getBoundingClientRect();
+    const x = e.clientX - rect.left; // x position within the element.
+    const width = rect.right - rect.left;
+    const part = Math.floor((x / width) * images.length);
+    setCurrentImage(images[part]);
+  };
+
+  const handleMouseLeave = () => {
+    setCurrentImage(images[0]);
+  };
 
   return (
     <Flex
@@ -20,9 +40,12 @@ function ThreeD() {
       <Divider />
       <Text size="5">Advanced Suit 2.0</Text>
       <img
-        src="images/3d/ps5_full_suit/portrait_annotated.png"
+        ref={imgRef}
+        src={currentImage}
         style={{ width: "100%" }}
         alt=""
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
       />
       {!isMobile && <Divider />}
       {!isMobile && (
