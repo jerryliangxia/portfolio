9e2c81801b75a6e46a6df0434a7b6ff7c042b157
Jerry Xia <jerryxia@Jerrys-MacBook-Pro.local>
2023-10-04 18:09:43 -0400

    Revert to old Toggle Suit button

--- src/components/3d/3dComponents/SpidermanShowcase.js
+++ src/components/3d/3dComponents/SpidermanShowcase.js
@@ -3,11 +3,9 @@ import { Canvas } from "@react-three/fiber";
 import MultiModelShowcase from "../MultiModelShowcase";
 import { Button, Flex } from "@radix-ui/themes";
 import { AnimatePresence, motion } from "framer-motion";
-import PSButton from "./PSButton";
 
 function SpidermanShowcase() {
   const [count, setCount] = useState(0);
-  const isMobile = window.innerWidth <= 768;
 
   return (
     <Flex className="flexContainer" align="center" gap="6" direction="column">
@@ -38,27 +36,13 @@ function SpidermanShowcase() {
           </motion.div>
         </AnimatePresence>
       </div>
-      {isMobile ? (
-        <Button
-          color={count % 2 === 0 ? "red" : "black"}
-          onClick={() => setCount((count + 1) % 2)}
-          radius="small"
-        >
-          Toggle Suit
-        </Button>
-      ) : (
-        <div
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          onClick={() => setCount((count + 1) % 2)}
-        >
-          <PSButton />
-        </div>
-      )}
+      <Button
+        color={count % 2 === 0 ? "red" : "black"}
+        onClick={() => setCount((count + 1) % 2)}
+        radius="small"
+      >
+        Toggle Suit
+      </Button>
     </Flex>
   );
 }
