eacfef5e2da0f96db1a0cdad5c8a22bd72570cbc
Jerry Xia <jerryxia@jerrys-mbp.home>
2023-10-05 09:40:51 -0400

    Add icons for SpecialContact on mobile to fix window bug

--- src/components/home/SpecialContact.js
+++ src/components/home/SpecialContact.js
@@ -2,8 +2,83 @@ import React from "react";
 import { Flex, Text, Link as RadixLink } from "@radix-ui/themes";
 import { flexGaps } from "../Info";
 
-function SpecialContact() {
+const iconSizeMobile = "80%";
+const SpecialContactMobile = () => {
   return (
+    <Flex gap={flexGaps.innerSectionFlexGap} align="start" direction="column">
+      <Text as="p">You can also find me here:</Text>
+      <Flex gap="4" justify="start" align="center" direction="row">
+        <RadixLink
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://sketchfab.com/jerrylxia"
+          size="2"
+        >
+          <img
+            src="images/about/sketchfab.png"
+            style={{ width: iconSizeMobile, objectFit: "contain" }}
+            alt=""
+          />
+        </RadixLink>
+        <RadixLink
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://jxia.artstation.com/"
+          size="2"
+        >
+          <img
+            src="images/about/artstation.png"
+            style={{ width: iconSizeMobile, objectFit: "contain" }}
+            alt=""
+          />
+        </RadixLink>
+        <RadixLink
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.behance.net/jerryxia?tracking_source=search_users|jerry%20xia"
+          size="2"
+        >
+          <img
+            src="images/about/behance.png"
+            style={{ width: iconSizeMobile, objectFit: "contain" }}
+            alt=""
+          />
+        </RadixLink>
+
+        <RadixLink
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://www.youtube.com/channel/UCItBzMg2Cw_xx8nSPjKWh1A"
+          size="2"
+        >
+          <img
+            src="images/about/youtube.png"
+            style={{ width: iconSizeMobile, objectFit: "contain" }}
+            alt=""
+          />
+        </RadixLink>
+        <RadixLink
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://jerrylxia.itch.io/"
+          size="2"
+        >
+          <img
+            src="images/about/itchio.png"
+            style={{ width: iconSizeMobile, objectFit: "contain" }}
+            alt=""
+          />
+        </RadixLink>
+      </Flex>
+    </Flex>
+  );
+};
+function SpecialContact() {
+  const isMobile = window.innerWidth <= 768;
+
+  return isMobile ? (
+    <SpecialContactMobile />
+  ) : (
     <Flex gap={flexGaps.innerSectionFlexGap} align="start" direction="column">
       <Text as="p">You can also find me here:</Text>
       <Flex gap="4" justify="start" align="center" direction="row">
